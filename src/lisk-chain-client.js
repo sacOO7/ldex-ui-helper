e6e509b630254624372fb6badccee44a5196d6ae
sacOO7 <sachinshinde7676@gmail.com>
2021-01-19 04:00:12 +0530

    Added methods to LiskChainClient, updated babelrc and tests for the same

--- src/lisk-chain-client.js
+++ src/lisk-chain-client.js
@@ -1,4 +1,28 @@
+import * as cryptography from '@liskhq/lisk-cryptography';
+import {Mnemonic} from "@liskhq/lisk-passphrase";
+import * as transactions from '@liskhq/lisk-transactions';
 
 export default class LiskChainClient {
-    
+
+    prepareTransaction = ( passphrase, amount, recipientAddress, data) => {
+        const amountInBeddows = transactions.utils.convertLSKToBeddows(amount).toString();
+        return transactions.transfer({
+            amount: amountInBeddows,
+            recipientId: recipientAddress,
+            data,
+            passphrase,
+        });
+    }
+
+    generatePassphrase = () => {
+        return Mnemonic.generateMnemonic();
+    }
+
+    isPassphraseValid = (passphrase) => {
+        return Mnemonic.validateMnemonic(passphrase, Mnemonic.wordlists.english);
+    }
+
+    getWalletDetails = (passphrase) => {
+        return cryptography.getAddressAndPublicKeyFromPassphrase(passphrase);
+    }
 }
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,7 +1,27 @@
+import LiskChainClient from "../src";
 
 
-describe("Index spec",() => {
-    it("Should pass the test", async () => {
-        expect("hello").toBe("hello")
+describe("Lisk Chain Client",() => {
+    it("Should generate passphrase",() => {
+        const passphrase = new LiskChainClient().generatePassphrase();
+        const words = passphrase.split(" ");
+        expect(words.length).toBeGreaterThan(8);
+        expect(passphrase).toBeDefined();
     });
+
+    it("Should validate passphrase",() => {
+        const lskClient = new LiskChainClient();
+        const passphrase = lskClient.generatePassphrase();
+        const isPassphraseValid = lskClient.isPassphraseValid(passphrase);
+        expect(isPassphraseValid).toBeTruthy();
+    });
+
+    it("should get wallet details", () => {
+        const lskClient = new LiskChainClient();
+        const passphrase = lskClient.generatePassphrase();
+        const { address, publicKey } = lskClient.getWalletDetails(passphrase);
+        expect(address).toBeTruthy();
+        expect(publicKey).toBeTruthy();
+    } )
+
 })
